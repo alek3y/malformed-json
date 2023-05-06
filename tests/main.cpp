2067f62c4c67f88bad0a68193c62df57647b44c8
alek3y <alek3y@protonmail.com>
2023-05-06 19:30:21 +0200

    Update and add tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -11,13 +11,10 @@ void parse(istream&);
 	do { \
 		stringstream stream(string((contents))); \
 		((void (*)(stringstream&)) (lambda))(stream); \
-		cout << "TEST:" << __LINE__ << ": Success" << endl; \
+		cout << "TEST:" << __LINE__ << ": Passed" << endl; \
 	} while (false);
 
-#define TEST_PARSER(contents) \
-	do { \
-		TEST((contents), [](auto s) {parse(s);}); \
-	} while (false);
+#define TEST_PARSER(contents) TEST((contents), [](auto s) {parse(s);})
 
 #define TEST_PARSER_THROW(contents, expected) \
 	do { \
@@ -25,40 +22,54 @@ void parse(istream&);
 			string msg; \
 			try { parse(s); } catch (json_exception e) { msg = e.msg; } \
 			if (msg != (expected)) { \
-				cout << "e.msg: '" << msg << "'" << endl; \
+				cout << "TEST:" << __LINE__ << ": e.msg: " << msg << endl; \
 			} \
 			assert(msg == (expected)); \
 		}); \
 	} while (false);
 
 void tests(void) {
-	TEST_PARSER_THROW("", "Unexpected empty file");
-	TEST_PARSER_THROW("A", "Expected primitive, but found byte 65");
-	TEST_PARSER_THROW("nul", "Expected 'null', but got 'nul'");
-	TEST_PARSER_THROW("nulu", "Expected 'null', but got 'nulu'");
+	TEST_PARSER_THROW("", "Unexpected EOF");
+	TEST_PARSER_THROW("A", "Expected primitive, got byte 65");
+	TEST_PARSER_THROW("nul", "Expected 'null', got 'nul'");
+	TEST_PARSER_THROW("nulu", "Expected 'null', got 'nulu'");
 	TEST_PARSER("null");
 
 	TEST_PARSER("3.14");
 
-	TEST_PARSER_THROW("f", "Expected 'false', but got 'f'");
-	TEST_PARSER_THROW("fals", "Expected 'false', but got 'fals'");
-	TEST_PARSER_THROW("flase", "Expected 'false', but got 'flase'");
+	TEST_PARSER_THROW("f", "Expected 'false', got 'f'");
+	TEST_PARSER_THROW("fals", "Expected 'false', got 'fals'");
+	TEST_PARSER_THROW("flase", "Expected 'false', got 'flase'");
 	TEST_PARSER("false");
 
-	TEST_PARSER_THROW("t", "Expected 'true', but got 't'");
-	TEST_PARSER_THROW("tre", "Expected 'true', but got 'tre'");
-	TEST_PARSER_THROW("treu", "Expected 'true', but got 'treu'");
+	TEST_PARSER("falseeeee");	// TODO: Should this work?
+
+	TEST_PARSER_THROW("t", "Expected 'true', got 't'");
+	TEST_PARSER_THROW("tre", "Expected 'true', got 'tre'");
+	TEST_PARSER_THROW("treu", "Expected 'true', got 'treu'");
 	TEST_PARSER("true");
 
-	TEST_PARSER_THROW("\"", "Expected end of string, got EOF");
-	TEST_PARSER_THROW("\"something", "Expected end of string, got EOF");
-	TEST_PARSER_THROW("\"\\\"", "Expected end of string, got EOF");
-	TEST_PARSER_THROW("\"something\\\"", "Expected end of string, got EOF");
+	TEST_PARSER_THROW("\"", "Expected '\"', got EOF");
+	TEST_PARSER_THROW("\"something", "Expected '\"', got EOF");
+	TEST_PARSER_THROW("\"\\\"", "Expected '\"', got EOF");
+	TEST_PARSER_THROW("\"something\\\"", "Expected '\"', got EOF");
 	TEST_PARSER("\"\"");
 	TEST_PARSER("\"\\\"\"");
 	TEST_PARSER("\"some \\\\\" thing\"");
 	TEST_PARSER("\"hello\nworld\"");
 	TEST_PARSER("\"some \\\"other\\\" string\"");
+
+	TEST_PARSER_THROW("[", "Unexpected EOF");
+	TEST_PARSER_THROW("[[", "Unexpected EOF");
+	TEST_PARSER_THROW("[\"something, 3.14]", "Expected '\"', got EOF");
+	TEST_PARSER_THROW("[tru]", "Expected 'true', got 'tru]'");
+	TEST_PARSER_THROW("[null, \"\", [1, 2]", "Expected ']', got ''");
+	TEST_PARSER_THROW("[,]", "Expected primitive, got byte 44");
+	TEST_PARSER("[]");
+	TEST_PARSER("[3.14]");
+	TEST_PARSER("[3.14,]");
+	TEST_PARSER("[\"something\", 3.14]");
+	TEST_PARSER("[true, false, \"\", [1, 2]]");
 }
 
 int main(void) {

d3c8be708284cc98785105c27880031a62e1f891
alek3y <alek3y@protonmail.com>
2023-05-06 19:29:46 +0200

    Add primitives and lists parser

--- src/json.cpp
+++ src/json.cpp
@@ -1,1 +1,98 @@
 #include "json.hpp"
+
+// Context-free grammar for the simplified JSON file type (see include/README.md):
+// <Json> → <Primitive> | [<List>] | {<Dict>}
+// <List> → ε | <Json> | <Json>,<List>
+// <Dict> → ε | <Pair> | <Pair>,<Dict>
+// <Primitive> → null | <Number> | <Bool> | "<Str>"
+//
+// <Pair> → ε | "<Str>":<Json>
+// <Str> → ε | <Char> | <Char><Str>
+// <Bool> → false | true
+//
+// where <Char> is a terminal with ascii from 0x20 to 0x7E with '"' = '\"',
+// and <Number> is a terminal represented as a double with no leading '.'
+
+static inline void parse_expect(std::istream& stream, std::string expected) {
+	std::string content;
+	content.resize(expected.length());
+
+	size_t bytes_read = stream.readsome(&content[0], expected.length());
+	content.resize(bytes_read);
+
+	if (content != expected) {
+		assert(!stream.eof());
+		throw json_exception{"Expected '" + expected + "', got '" + content + "'"};
+	}
+}
+
+static void parse_primitive(std::istream& stream) {
+	char symbol = 0;
+	stream >> symbol;
+
+	if (symbol == '"') {
+		std::string content;
+		do {
+			content += stream.get();
+			if (!stream) {
+				throw json_exception{"Expected '\"', got EOF"};
+			}
+		} while (content.back() != '"' || (content.length() > 1 && content[content.length()-2] == '\\'));
+		content.pop_back();
+	} else {
+		stream.putback(symbol);
+
+		if (symbol == 'n') {
+			parse_expect(stream, "null");
+		} else if (symbol >= '0' && symbol <= '9') {
+			double number;
+			stream >> number;
+		} else if (symbol == 'f') {
+			parse_expect(stream, "false");
+		} else if (symbol == 't') {
+			parse_expect(stream, "true");
+		} else {
+			throw json_exception{"Expected primitive, got byte " + std::to_string((int) symbol)};
+		}
+	}
+}
+
+void parse(std::istream& stream);
+
+static void parse_list(std::istream& stream) {
+	char symbol = 0;
+	stream >> symbol;
+	stream.putback(symbol);
+
+	if (symbol != ']') {
+		parse(stream);
+
+		stream >> symbol;
+		if (symbol == ',') {
+			parse_list(stream);
+		} else {
+			stream.putback(symbol);
+		}
+	}
+}
+
+// Corresponds to the <Json> non-terminal
+// TODO: Change to operator>>
+void parse(std::istream& stream) {
+	char symbol = 0;
+	stream >> symbol;
+	if (!stream) {
+		throw json_exception{"Unexpected EOF"};
+	}
+
+	if (symbol == '[') {
+		parse_list(stream);
+		parse_expect(stream, "]");
+	} else if (symbol == '{') {
+		// TODO: <Dict>
+		parse_expect(stream, "}");
+	} else {
+		stream.putback(symbol);
+		parse_primitive(stream);
+	}
+}

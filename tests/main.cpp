669623948094d0d855eb1ea78039197f7ebf2aa3
alek3y <alek3y@protonmail.com>
2023-05-06 15:37:23 +0200

    Add test routines

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,48 @@
 #include "json.hpp"
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <functional>
+using namespace std;
+
+void parse(istream&);
+
+void test(string contents, bool throws, bool (*lambda)(std::istream&), unsigned line) {
+	string msg;
+	bool success;
+	try {
+		stringstream stream(contents);
+		success = lambda(stream) && !throws;
+	} catch (json_exception e) {
+		msg = e.msg;
+		success = throws;
+	}
+
+	cout << "TEST:" << line << ": ";
+	if (success) {
+		cout << "Success";
+	} else {
+		cout << "Fail";
+	}
+
+	if (msg.length() > 0) {
+		cout << " => " << msg;
+	}
+	cout << endl;
+}
+
+void test_parse(string contents, bool throws, unsigned line) {
+	test(contents, throws, [](auto s) -> bool {
+		parse(s);
+		return true;
+	}, line);
+}
+
+#define TEST(contents, throws, lambda) test(contents, throws, lambda, __LINE__)
+#define TEST_PARSE(contents, throws) test_parse(contents, throws, __LINE__)
 
 int main(void) {
+	TEST_PARSE("", true);
+	TEST_PARSE("nul", true);
+	TEST_PARSE("null", false);
 }

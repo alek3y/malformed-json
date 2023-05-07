6662857cf410e7903b91a1f142f2cd75a942cf17
alek3y <alek3y@protonmail.com>
2023-05-07 20:01:27 +0200

    Add more tests

--- tests/main.cpp
+++ tests/main.cpp
@@ -95,28 +95,58 @@ void tests(void) {
 	TEST_PARSER("{\"pi\": 3.14}");
 	TEST_PARSER("{\"key\": [1, {\"a\": \"b\", \"c\": 3.14}, 2], }");
 
-	// TODO: Test this and move constructor with actual data
-	TEST("", [](auto s) {
+	TEST("\"first\"[2]", [](auto s) {
 		json j1;
-		{
-			json j2(j1);
-		}
-		assert(j1.is_null());
-	});
-
-	TEST("", [](auto s) {
-		json j1;
+		s >> j1;
 		{
 			json j2;
-			j1 = move(j2);
+			s >> j2;
+
+			assert(j2.is_list());
+			auto it = j2.begin_list();
+			assert(it->is_number() && it->get_number() == 2.0);
+			assert((++it) == j2.end_list());
+
+			j2 = j1;
+			assert(j2.is_string() && j2.get_string() == "first");
 		}
-		assert(j1.is_null());
+		assert(j1.is_string() && j1.get_string() == "first");
 	});
 
-	TEST("", [](auto s) {
+	TEST("false\"false\"", [](auto s) {
+		json j1;
+		s >> j1;
+		assert(j1.is_bool() && j1.get_bool() == false);
+		{
+			json j2;
+			s >> j2;
+			assert(j2.is_string() && j2.get_string() == "false");
+			j1 = move(j2);
+		}
+		assert(j1.is_string() && j1.get_string() == "false");
+	});
+
+	TEST("null", [](auto s) {
+		json j;
+		s >> j;
+		assert(j.is_null());
+
 		string msg;
 		try {
-			json j;
+			j.insert(pair<string, json>("hello", j));
+		} catch (json_exception e) {
+			msg = e.msg;
+		}
+		assert(msg == "Wrong json& type for insert");
+	});
+
+	TEST("3.14", [](auto s) {
+		json j;
+		s >> j;
+		assert(j.is_number() && j.get_number() == 3.14);
+
+		string msg;
+		try {
 			j["test"];
 		} catch (json_exception e) {
 			msg = e.msg;
@@ -124,36 +154,70 @@ void tests(void) {
 		assert(msg == "Wrong json& type for operator[]");
 	});
 
-	TEST("", [](auto s) {
-		string msg;
-		try {
-			json j1;
-			{
-				const json& j2 = j1;
-				j2["test"];
-			}
-		} catch (json_exception e) {
-			msg = e.msg;
-		}
-		assert(msg == "Wrong const json& type for operator[]");
-	});
-
-	TEST("", [](auto s) {
-		string msg;
-		try {
-			json j;
-			j.insert(pair<string, json>());
-		} catch (json_exception e) {
-			msg = e.msg;
-		}
-		assert(msg == "Wrong json& type for insert");
-	});
-
-	TEST("", [](auto s) {
+	TEST("{}", [](auto s) {
 		json j;
-		j.set_dictionary();
-		j.insert(pair<string, json>());
-		assert(j.is_dictionary());
+		s >> j;
+		assert(j.is_dictionary() && j.begin_dictionary() == j.end_dictionary());
+
+		j["test"];
+		auto it = j.begin_dictionary();
+		assert((*it).first == "test" && (*it).second.is_null());
+	});
+
+	TEST("{}", [](auto s) {
+		json j1;
+		s >> j1;
+		assert(j1.is_dictionary() && j1.begin_dictionary() == j1.end_dictionary());
+
+		string msg;
+		try {
+			const json& j2 = j1;
+			j2["test"];
+		} catch (json_exception e) {
+			msg = e.msg;
+		}
+		assert(msg == "Unable to create key 'test' for const json&");
+	});
+
+	TEST("[1,]", [](auto s) {
+		json j1;
+		s >> j1;
+		assert(j1.is_list());
+
+		auto it1a = j1.begin_list();
+		auto it1b = it1a++;
+		assert(it1a == j1.end_list() && it1b == j1.begin_list());
+
+		json j2(j1);
+		json value;
+		value.set_number(2.0);
+		j2.push_back(value);
+		assert((++it1b) == j1.end_list());
+
+		auto it2 = j2.begin_list();
+		assert((*it2).is_number() && it2->get_number() == 1.0);
+		it2++;
+		assert(&(*it2) != &value);
+		assert(it2->is_number() && it2->get_number() == 2.0);
+	});
+
+	TEST("[1, 2]", [](auto s) {
+		json jl;
+		s >> jl;
+		assert(jl.is_list());
+
+		auto it = jl.begin_list();
+		json j1 = *(it++);
+		assert(j1.is_number() && j1.get_number() == 1.0);
+
+		json j2 = *(it++);
+		assert(j2.is_number() && j2.get_number() == 2.0);
+		assert(it == jl.end_list());
+
+		double* j1ptr = &j1.get_number();
+		double* j2ptr = &j2.get_number();
+		j1 = move(j2);
+		assert(&j1.get_number() == j2ptr && j2ptr != j1ptr);
 	});
 
 	TEST("", [](auto s) {
@@ -180,8 +244,8 @@ void tests(void) {
 			j2["str"].set_string("hello");
 			j1 = move(j2);
 		}
-		const json& j2 = j1;
-		assert(j1["null"].is_null() && j2["str"].is_string() && j1["str"].get_string() == "hello");
+		assert(j1["null"].is_null());
+		assert(j1["str"].is_string() && j1["str"].get_string() == "hello");
 	});
 
 	TEST("", [](auto s) {
@@ -203,11 +267,35 @@ void tests(void) {
 		assert(j.is_list());
 
 		j.set_dictionary();
-		j.insert(make_pair("hello", e));
+		j.insert(pair<string, json>("hello", e));
 		assert(j.is_dictionary() && j["hello"].get_string() == "world");
 
 		j.set_null();
 		assert(j.is_null());
+	});
+
+	TEST("[1, 2, 3]", [](auto s) {
+		json j;
+		s >> j;
+		assert(j.is_list());
+
+		auto it = j.begin_list();
+		assert(it->is_number() && it->get_number() == 1.0);
+
+		(*(++it)).get_number() *= 2.0;
+		assert(it->is_number() && (*(it++)).get_number() == 4.0);
+
+		assert(it->is_number() && it->get_number() == 3.0);
+	});
+
+	TEST("1", [](auto s) {
+		json j1;
+		s >> j1;
+		j1 = j1;
+		const json& j2 = j1;
+		j1 = j2;
+		j1 = move(j1);
+		assert(j1.is_number() && j1.get_number() == 1.0);
 	});
 }
 
